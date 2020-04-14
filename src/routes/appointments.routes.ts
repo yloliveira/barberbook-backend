6424f78c8b4c245d84025b555229735da70d9d57
Yan Oliveira <oliveira.yan02@gmail.com>
2020-04-14 16:40:16 -0300

    Add date validation

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,18 +1,38 @@
 import { Router } from 'express';
 import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
+
+interface Appointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
 
 const routes = Router();
 
-const appointments = [];
+const appointments: Appointment[] = [];
 
 routes.post('/', (req, res) => {
-  const { provider, data } = req.body;
+  const { provider, date } = req.body;
+  const parsedDate = startOfHour(parseISO(date));
+  const findAppointmentInSameDate = appointments.find(appointment =>
+    isEqual(parsedDate, appointment.date),
+  );
+
+  if (findAppointmentInSameDate) {
+    return res
+      .status(400)
+      .json({ message: 'This appointment is already booked' });
+  }
+
   const appointment = {
     id: uuid(),
     provider,
-    data,
+    date: parsedDate,
   };
+
   appointments.push(appointment);
+
   return res.json(appointment);
 });
 
