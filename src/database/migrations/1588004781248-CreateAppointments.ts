abf3d0c5bec00aa71500458b016d1a8fa2fee14b
Yan Oliveira <oliveira.yan02@gmail.com>
2020-04-27 13:59:02 -0300

    Fix migration

--- src/database/migrations/1588004781248-CreateAppointments.ts
+++ src/database/migrations/1588004781248-CreateAppointments.ts
@@ -11,6 +11,7 @@ export class CreateAppointments1588004781248 implements MigrationInterface {
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
