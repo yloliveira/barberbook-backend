b10944d7d135e5b7dce1ba6aef52cb54f12eb977
Yan Oliveira <oliveira.yan02@gmail.com>
2020-04-28 10:42:13 -0300

    refactor: improvement in the model files

--- src/models/appointment.model.ts
+++ src/models/appointment.model.ts
@@ -1,4 +1,10 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity('appointments')
 export default class Appointment {
@@ -11,9 +17,9 @@ export default class Appointment {
   @Column('timestamp with time zone')
   date: Date;
 
-  @Column('timestamp')
+  @CreateDateColumn()
   created_at: Date;
 
-  @Column('timestamp')
+  @UpdateDateColumn()
   updated_at: Date;
 }
--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,4 +1,10 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity('users')
 export default class User {
@@ -14,9 +20,9 @@ export default class User {
   @Column()
   password: string;
 
-  @Column('timestamp')
+  @CreateDateColumn()
   created_at: Date;
 
-  @Column('timestamp')
+  @UpdateDateColumn()
   updated_at: Date;
 }
