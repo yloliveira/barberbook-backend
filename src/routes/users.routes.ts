24a9dbb0ae072ab53a51260fe9037b0c2544ac63
Yan Oliveira <oliveira.yan02@gmail.com>
2020-04-28 11:34:45 -0300

    refactor: ensure response does not contain the password

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -7,6 +7,7 @@ routes.post('/', async (req, res) => {
   try {
     const createUser = new CreateUserService();
     const user = await createUser.execute(req.body);
+    delete user.password;
     return res.json(user);
   } catch (err) {
     return res.status(400).json({ error: err.message });
