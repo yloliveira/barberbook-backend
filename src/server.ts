5ce0870b2e6c39d9af147a83192de1b3dd1fb3a8
Yan Oliveira <oliveira.yan02@gmail.com>
2020-05-05 19:41:20 -0300

    feat: add static route to files

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import routes from './routes';
 import './database';
+import updloadConfig from './config/upload';
 
 const app = express();
 app.use(express.json());
+app.use('/files', express.static(updloadConfig.directory));
 app.use(routes);
 
 app.listen(3333, () => {
