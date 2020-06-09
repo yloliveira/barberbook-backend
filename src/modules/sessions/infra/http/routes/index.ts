e18f8c0a6b3eb4de730338c88b6f89943c68b4cd
Yan Oliveira <oliveira.yan02@gmail.com>
2020-06-09 15:48:52 -0300

    refactor: move sessions to new module

--- src/modules/sessions/infra/http/routes/index.ts
+++ src/modules/sessions/infra/http/routes/index.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
-import CreateSessionService from '@modules/users/services/createSession';
+import CreateService from '@modules/sessions/services/create';
 
 const routes = Router();
 
 routes.post('/', async (req, res) => {
-  const createSession = new CreateSessionService();
+  const createSession = new CreateService();
   const { email, password } = req.body;
   const session = await createSession.execute({ email, password });
   return res.json(session);
