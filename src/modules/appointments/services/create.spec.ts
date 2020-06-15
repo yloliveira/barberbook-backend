ceb1075b7ae5f8c4d5e4ad7ae499008d421885a8
Yan Oliveira <oliveira.yan02@gmail.com>
2020-06-15 13:51:42 -0300

    test: should not be able to create a new appointment in same date

--- src/modules/appointments/services/create.spec.ts
+++ src/modules/appointments/services/create.spec.ts
@@ -1,5 +1,6 @@
 import Create from './create';
 import Repository from '../mocks/Repository';
+import AppError from '@shared/errors/appError';
 
 describe('Create', () => {
   test('should be able to create a new appointment', async () => {
@@ -12,5 +13,18 @@ describe('Create', () => {
 
     expect(appointment).toHaveProperty('id');
     expect(appointment.provider_id).toBe('valid_provider_id');
+  });
+
+  test('should not be able to create a new appointment in same date', async () => {
+    const repository = new Repository();
+    const create = new Create(repository);
+    const date = new Date();
+    const provider_id = 'valid_provider_id';
+
+    await create.execute({ provider_id, date });
+
+    expect(create.execute({ provider_id, date })).rejects.toBeInstanceOf(
+      AppError,
+    );
   });
 });
