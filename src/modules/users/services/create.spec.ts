75e2c9190288a3630120c7fc655e6b510d412993
Yan Oliveira <oliveira.yan02@gmail.com>
2020-06-15 14:23:43 -0300

    test: should not be able to create a new user with email address already used

--- src/modules/users/services/create.spec.ts
+++ src/modules/users/services/create.spec.ts
@@ -1,5 +1,6 @@
 import Create from './create';
 import Repository from '../mocks/Repository';
+import AppError from '@shared/errors/appError';
 
 describe('Create', () => {
   test('should be able to create a new user', async () => {
@@ -14,5 +15,19 @@ describe('Create', () => {
     expect(user).toHaveProperty('id');
     expect(user.name).toBe('valid_name');
     expect(user.email).toBe('valid@email.com');
+  });
+
+  test('should not be able to create a new user with email address already used', async () => {
+    const repository = new Repository();
+    const create = new Create(repository);
+    const user = {
+      name: 'valid_name',
+      email: 'valid@email.com',
+      password: 'valid_password',
+    };
+
+    await create.execute(user);
+
+    expect(create.execute(user)).rejects.toBeInstanceOf(AppError);
   });
 });
