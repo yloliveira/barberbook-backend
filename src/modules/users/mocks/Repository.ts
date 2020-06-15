705cd9b99d98d8ec732020aed9f22f638ef47872
Yan Oliveira <oliveira.yan02@gmail.com>
2020-06-15 14:00:23 -0300

    feat: add mocked users repository

---
+++ src/modules/users/mocks/Repository.ts
@@ -0,0 +1,30 @@
+import IRepository from '../interfaces/IRepository';
+import ICreateUser from '../interfaces/ICreateUser';
+import User from '../infra/typeorm/entities';
+import { uuid } from 'uuidv4';
+
+export default class UserRepository implements IRepository {
+  private users: User[] = [];
+
+  public async findByEmail(email: string): Promise<User | undefined> {
+    const user = this.users.find(user => user.email === email);
+    return user;
+  }
+
+  public async findById(id: string): Promise<User | undefined> {
+    const user = this.users.find(user => user.id === id);
+    return user;
+  }
+
+  public async create(userData: ICreateUser): Promise<User> {
+    const user = new User();
+    Object.assign(user, { id: uuid(), ...userData });
+    return user;
+  }
+
+  public async save(user: User): Promise<User> {
+    const userIndex = this.users.findIndex(({ id }) => id === user.id);
+    this.users[userIndex] = user;
+    return user;
+  }
+}
