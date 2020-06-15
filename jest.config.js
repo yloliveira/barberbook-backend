c881b23010390ccefadb8d57cbb6c3822782c596
Yan Oliveira <oliveira.yan02@gmail.com>
2020-06-15 11:10:54 -0300

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+
+module.exports = {
+  clearMocks: true,
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/*.spec.ts'],
+};
